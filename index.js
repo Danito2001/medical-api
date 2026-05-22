6cbb180ed4f6084949a375b31c743722fc236fbc
Daniel Carvajal <107225266+Danito2001@users.noreply.github.com>
2026-05-21 23:24:02 -0400

    change origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3001;
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: 'https://dani-medicalschedule.netlify.app',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
